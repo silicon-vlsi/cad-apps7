474196889fe0f5f9a565d82de8978f15074cd384
SRV01 Admin <vlsi@silicon.ac.in>
2022-03-11 16:50:55 +0530

    updates..

--- bin/patch-localquota.sh
+++ bin/patch-localquota.sh
@@ -8,6 +8,12 @@ echo "*********************************************"
 #Check if /home/local/simulation exists
 if [ -d /home/local/simulation ];
  then
+   ##Sometimes the kickstarter is not setting the permissions
+   ##properly. So redoing it just to make sure
+   echo "Setting permissions"
+   chgrp localsim /home/local/simulation
+   chmod g=swrx,+t /home/local/simulation
+   # Now set the quota
    echo "Adding quota for /home/local/simulation..."
    xfs_quota -x -c 'project -s localsim' /home
    xfs_quota -x -c 'limit -p bsoft=95G bhard=100G localsim' /home
@@ -16,5 +22,17 @@ if [ -d /home/local/simulation ];
    xfs_quota -x -c 'report -h' /home
  else
    echo "ERROR: /home/local/simulation DOES NOT EXIST" 
+   echo "CREATING ...."
    echo ""
+   mkdir -p /home/local/simulation
+   echo "Setting Permissions"
+   chgrp localsim /home/local/simulation
+   chmod g=swrx,+t /home/local/simulation
+   # Now set the quota
+   echo "Adding quota for /home/local/simulation..."
+   xfs_quota -x -c 'project -s localsim' /home
+   xfs_quota -x -c 'limit -p bsoft=95G bhard=100G localsim' /home
+   echo ""
+   echo "Quota report for /home..."
+   xfs_quota -x -c 'report -h' /home
 fi
